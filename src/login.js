5f9e901dd7a1c87061a3c07282304086e92c77c5
ukss <86929961+ukssss@users.noreply.github.com>
2022-10-24 04:06:21 +0900

    #6 todo.js 구현
    todo-list 구현, 로그인 시 나타나며, 체크박스, 텍스트, 버튼 형식으로 구현

--- src/login.js
+++ src/login.js
@@ -1,13 +1,14 @@
+const todoDiv = document.querySelector("#todo-div");
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector(".login__input");
 
-const userDiv = document.querySelector("#user-div");
+const greetingDiv = document.querySelector("#greeting-div");
 
 const USERNAME_KEY = "username";
 const HIDDEN_CLASS = "hidden";
 
-function onLoginSubmit(e) {
-  e.preventDefault();
+function onLoginSubmit(event) {
+  event.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASS);
   localStorage.setItem(USERNAME_KEY, username);
@@ -15,8 +16,9 @@ function onLoginSubmit(e) {
 }
 
 function onEnterUserspace(username) {
-  userDiv.classList.remove(HIDDEN_CLASS);
-  userDiv.innerHTML = `<span class="user__greeting">Good Morning ${username}!</span>`;
+  todoDiv.classList.remove(HIDDEN_CLASS);
+  greetingDiv.classList.remove(HIDDEN_CLASS);
+  greetingDiv.innerHTML = `<span class="greeting__text">Good Morning ${username}!</span>`;
 }
 
 const saveUsername = localStorage.getItem(USERNAME_KEY);
---
+++ src/todo.js
@@ -0,0 +1,57 @@
+const todoInput = todoDiv.querySelector(".todo__input");
+const todoList = todoDiv.querySelector(".todo__list");
+
+const TODOS_KEY = "todos";
+let todos = [];
+
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+  saveTodos();
+}
+
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+
+  li.appendChild(checkBox);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  todoList.appendChild(li);
+}
+
+function handlerTodoSubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  todos.push(newTodoObj);
+  paintTodo(newTodoObj);
+  saveTodos();
+}
+
+todoDiv.addEventListener("submit", handlerTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
